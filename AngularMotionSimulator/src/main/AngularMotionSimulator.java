b92315c00f7dcc0d85206a0b5bd30ba90a21fba3
Bryan Kristiono <ben2616@hotmail.com>
2015-12-05 15:11:46 -0500

    test

---
+++ AngularMotionSimulator/src/main/AngularMotionSimulator.java
@@ -0,0 +1,16 @@
+/**AngularMotionSimulator
+ * A Main project software that creates an animation of a spinning circle
+ * With user-inputted variables that could change the circle's properties
+ * 
+ */
+
+package main;
+
+public class AngularMotionSimulator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
