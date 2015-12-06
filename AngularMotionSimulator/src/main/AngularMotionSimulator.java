ef9586b6d4b1bac0566b41433740f291b01aeae5
Amritpal Aujla <amritprabh98@hotmail.com>
2015-12-05 19:49:37 -0500

    Experience the glory of CTHULHUUUUUUUUUUUUUUUUUUUUUU

--- AngularMotionSimulator/src/main/AngularMotionSimulator.java
+++ AngularMotionSimulator/src/main/AngularMotionSimulator.java
@@ -11,6 +11,7 @@ public class AngularMotionSimulator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("CTHU:HU == NICOLAS CAGE == THE ONION == THE LOTUS");
 	}
 
 }
