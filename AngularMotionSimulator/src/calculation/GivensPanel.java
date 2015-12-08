538ea124565655675a8b07049c625892d2751324
Amritpal Aujla <amritprabh98@hotmail.com>
2015-12-07 22:22:11 -0500

    calculation stubs finished

--- AngularMotionSimulator/src/calculation/GivensPanel.java
+++ AngularMotionSimulator/src/calculation/GivensPanel.java
@@ -1,5 +1,31 @@
 package calculation;
 
+import javax.swing.*;
+
 public class GivensPanel {
+	private Calculations calculations;
+	private JLabel angularVelocity;
+	private JLabel linearVelocity;
+	private JLabel radius;
+	private JLabel time;
+	private JLabel angle;
+	private JLabel arcLength;
 	
+	public GivensPanel(Calculations calculations){
+		this.calculations = calculations;
+		this.layoutView();
+	}
+	
+	public void update(){
+		
+	}
+	
+	private void registerControllers(){
+		
+	}
+	
+	private void layoutView(){
+		
+	}
+
 }
