610330a2e9cfe49d30ecf0c83a6f8881770111c6
Amritpal Aujla <amritprabh98@hotmail.com>
2015-12-07 22:18:00 -0500

    finished calculation stubs

---
+++ AngularMotionSimulator/src/calculation/CalculationsPanel.java
@@ -0,0 +1,25 @@
+package calculation;
+
+import javax.swing.*;
+
+public class CalculationsPanel {
+	
+	private Calculations calculations;
+	private JLabel wantedLabel;
+	private JLabel calcFormulas;
+	private JLabel calcWithValues;
+	private JLabel calcAnswer;
+	
+	public CalculationsPanel(Calculations calculations){
+		this.calculations = calculations;
+		this.layoutView();
+	}
+	
+	public void update(){
+		
+	}
+	
+	private void layoutView(){
+		
+	}
+}
--- AngularMotionSimulator/src/calculation/ProcessFrame.java
+++ AngularMotionSimulator/src/calculation/ProcessFrame.java
@@ -1,5 +1,28 @@
 package calculation;
 
+import javax.swing.*;
+
 public class ProcessFrame {
+	private Calculations calculations;
+	private GivensPanel givensPanel;
+	private CalculationsPanel calcPanel;
+	private JButton save;
 	
+	public ProcessFrame(Calculations calculations){
+		this.calculations = calculations;
+		this.layoutView();
+		this.registerControllers();
+	}
+	
+	public void update(){
+		
+	}
+	
+	private void layoutView(){
+		
+	}
+	
+	private void registerControllers(){
+		
+	}
 }
