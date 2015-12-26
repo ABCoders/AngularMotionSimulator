9c7851b3dddfc088a9e374c1fa088a8b85cc85d0
Amritpal Aujla <amritprabh98@hotmail.com>
2015-12-26 18:49:45 -0500

    removed extra html tags from the error messages

--- AngularMotionSimulator/src/calculation/Calculations.java
+++ AngularMotionSimulator/src/calculation/Calculations.java
@@ -436,17 +436,17 @@ public class Calculations{
 		//sets the error message as which variables the user needs to calculate for the wanted variable that the user has input
 		switch(this.wantedVariable){
 			case("Angular Velocity"):
-				error = "To calculate Angular Velocity, you need either of these sets of measurements:<p></p> <p>Angle and Time <p>Linear Velocity and Radius <p>Arc Length, Angle, and Linear Velocity <p>Time, Radius, and Arc Length</p></p></p></p>";
+				error = "To calculate Angular Velocity, you need either of these sets of measurements: Angle and Time <p>Linear Velocity and Radius <p>Arc Length, Angle, and Linear Velocity <p>Time, Radius, and Arc Length</p></p></p></p>";
 			case("Linear Velocity"):
-				error = "To calculate Linear Velocity, you need either of these sets of measurements:<p></p> <p>Arc Length and Time <p>Angular Velocity and Radius <p>Arc Length, Angle, and Angular Velocity <p>Time, Radius, and Angle</p></p></p></p>";
+				error = "To calculate Linear Velocity, you need either of these sets of measurements: <p>Arc Length and Time <p>Angular Velocity and Radius <p>Arc Length, Angle, and Angular Velocity <p>Time, Radius, and Angle</p></p></p></p>";
 			case("Radius"):
-				error = "To calculate Radius, you need either of these sets of measurements:<p></p> <p>Linear Velocity and Angular Velocity <p>Arc Length and Angle <p>Arc Length, Time, and Angular Velocity <p>Time, Angle, and Linear Velocity</p></p></p></p>";
+				error = "To calculate Radius, you need either of these sets of measurements: <p>Linear Velocity and Angular Velocity <p>Arc Length and Angle <p>Arc Length, Time, and Angular Velocity <p>Time, Angle, and Linear Velocity</p></p></p></p>";
 			case("Time"):
-				error = "To calculate Time, you need either of these sets of measurements:<p></p> <p>Angular Velocity and Angle <p>Linear Velocity and Arc Length <p>Radius, Angle, and Linear Velocity <p>Angular Velocity, Radius, and Arc Length</p></p></p></p>";
+				error = "To calculate Time, you need either of these sets of measurements: <p>Angular Velocity and Angle <p>Linear Velocity and Arc Length <p>Radius, Angle, and Linear Velocity <p>Angular Velocity, Radius, and Arc Length</p></p></p></p>";
 			case("Arc Length"):
-				error = "To calculate Arc Length you need either of these sets of measurements:<p></p> <p>Time and Linear Velocity <p>Angle and Radius <p>Radius, Time, and Angular Velocity <p>Angular Velocity, Angle, and Angular Velocity</p></p></p></p>";
+				error = "To calculate Arc Length you need either of these sets of measurements: <p>Time and Linear Velocity <p>Angle and Radius <p>Radius, Time, and Angular Velocity <p>Angular Velocity, Angle, and Angular Velocity</p></p></p></p>";
 			case("Angle"):
-				error = "To calculate Angle, you need either of these sets of measurements:<p></p> <p>Angular Velocity and Time <p>Radius and Arc Length <p>Arc Length, Angular Velocity, and Linear Velocity <p>Time, Radius, and Linear Velocity</p></p></p></p>";
+				error = "To calculate Angle, you need either of these sets of measurements: <p>Angular Velocity and Time <p>Radius and Arc Length <p>Arc Length, Angular Velocity, and Linear Velocity <p>Time, Radius, and Linear Velocity</p></p></p></p>";
 		}
 		
 		return error;
