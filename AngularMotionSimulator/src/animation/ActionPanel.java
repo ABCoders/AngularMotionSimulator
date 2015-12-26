7ad08979773cf05b8a05c94ccd59a81698f07660
Bryan Kristiono <ben2616@hotmail.com>
2015-12-25 21:12:29 -0500

    Made play and pause button to one button

--- AngularMotionSimulator/src/animation/ActionPanel.java
+++ AngularMotionSimulator/src/animation/ActionPanel.java
@@ -11,13 +11,12 @@ import javax.swing.*;
 public class ActionPanel extends JPanel {
 	private Animation animation;	//The model of the animation
 	
-	private JButton playButton;		//The button that starts the animation
-	private JButton pauseButton;	//The button that stops the animation
-	private JButton clearButton;	//The button that resets the drawn point
-	private JLabel timeLabel;		//The label to indicate which slider is for time
-	private JLabel scaleLabel;		//The label to indicate which slider is for scaling
-	private JSlider timeSlider;		//The slider that changes the time of the animation
-	private JSlider scaleSlider;	//The slider that changes the scale of the animation
+	private JButton animationButton;	//The button that starts and stops the animation
+	private JButton clearButton;		//The button that resets the drawn point
+	private JLabel timeLabel;			//The label to indicate which slider is for time
+	private JLabel scaleLabel;			//The label to indicate which slider is for scaling
+	private JSlider timeSlider;			//The slider that changes the time of the animation
+	private JSlider scaleSlider;		//The slider that changes the scale of the animation
 	
 	private int max;
 	/**
@@ -39,8 +38,7 @@ public class ActionPanel extends JPanel {
 	 */
 	private void createComponents() {
 		//Buttons
-		playButton = new JButton("PLAY");
-		pauseButton = new JButton("PAUSE");
+		animationButton = new JButton("PLAY");
 		clearButton = new JButton("CLEAR");
 		
 		//Labels
@@ -73,8 +71,7 @@ public class ActionPanel extends JPanel {
 		this.add(scaleSlider);
 		this.add(timeLabel);
 		this.add(timeSlider);
-		this.add(playButton);
-		this.add(pauseButton);
+		this.add(animationButton);
 		this.add(clearButton);
 	}
 	
@@ -82,8 +79,7 @@ public class ActionPanel extends JPanel {
 	 * Add Listeners to all components
 	 */
 	private void registerControllers() {
-		playButton.addActionListener(new ActionController(animation));
-		pauseButton.addActionListener(new ActionController(animation));
+		animationButton.addActionListener(new ActionController(animation));
 		clearButton.addActionListener(new ActionController(animation));
 			
 		timeSlider.addChangeListener(new TimeSliderController(animation));
@@ -142,14 +138,12 @@ public class ActionPanel extends JPanel {
 		}
 		
 		if (animation.getState()) {
-			playButton.setEnabled(false);
-			pauseButton.setEnabled(true);
+			animationButton.setText("PAUSE");
 			timeSlider.setValue((int)Math.round(animation.getTime()*10));
 			scaleSlider.setValue((int)(animation.getScale()*100));
 		}
 		else {
-			playButton.setEnabled(true);
-			pauseButton.setEnabled(false);
+			animationButton.setText("PLAY   ");
 		}
 	}
 }
