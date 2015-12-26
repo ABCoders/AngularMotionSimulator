94da05cf2f533d8aa8f878ea9aad0b2d1b8d03ac
Amritpal Aujla <amritprabh98@hotmail.com>
2015-12-26 00:16:17 -0500

    got calculations error working, made logo equilateral

--- AngularMotionSimulator/src/calculation/Calculations.java
+++ AngularMotionSimulator/src/calculation/Calculations.java
@@ -386,17 +386,17 @@ public class Calculations{
 	private String giveRelevantError(){
 		switch(this.wantedVariable){
 			case("Angular Velocity"):
-				return "To calculate Angular Velocity, you need either of these sets of measurements: \nAngle and Time \nLinear Velocity and Radius \nArc Length, Angle, and Linear Velocity \nTime, Radius, and Arc Length";
+				return "To calculate Angular Velocity, you need either of these sets of measurements:<p></p> <p>Angle and Time <p>Linear Velocity and Radius <p>Arc Length, Angle, and Linear Velocity <p>Time, Radius, and Arc Length</p></p></p></p>";
 			case("Linear Velocity"):
-				return "To calculate Linear Velocity, you need either of these sets of measurements: \nAngle and Time \nLinear Velocity and Radius \nArc Length, Angle, and Linear Velocity \nTime, Radius, and Arc Length";
+				return "To calculate Linear Velocity, you need either of these sets of measurements:<p></p> <p>Arc Length and Time <p>Angular Velocity and Radius <p>Arc Length, Angle, and Angular Velocity <p>Time, Radius, and Angle</p></p></p></p>";
 			case("Radius"):
-				return "To calculate Radius, you need either of these sets of measurements: \nAngle and Time \nLinear Velocity and Radius \nArc Length, Angle, and Linear Velocity \nTime, Radius, and Arc Length";
+				return "To calculate Radius, you need either of these sets of measurements:<p></p> <p>Linear Velocity and Angular Velocity <p>Arc Length and Angle <p>Arc Length, Time, and Angular Velocity <p>Time, Angle, and Linear Velocity</p></p></p></p>";
 			case("Time"):
-				return "To calculate Time, you need either of these sets of measurements: \nAngle and Time \nLinear Velocity and Radius \nArc Length, Angle, and Linear Velocity \nTime, Radius, and Arc Length";
+				return "To calculate Time, you need either of these sets of measurements:<p></p> <p>Angular Velocity and Angle <p>Linear Velocity and Arc Length <p>Radius, Angle, and Linear Velocity <p>Angular Velocity, Radius, and Arc Length</p></p></p></p>";
 			case("Arc Length"):
-				return "To calculate Arc Length you need either of these sets of measurements: \nAngle and Time \nLinear Velocity and Radius \nArc Length, Angle, and Linear Velocity \nTime, Radius, and Arc Length";
+				return "To calculate Arc Length you need either of these sets of measurements:<p></p> <p>Time and Linear Velocity <p>Angle and Radius <p>Radius, Time, and Angular Velocity <p>Angular Velocity, Angle, and Angular Velocity</p></p></p></p>";
 			case("Angle"):
-				return "To calculate Angle, you need either of these sets of measurements: \nAngle and Time \nLinear Velocity and Radius \nArc Length, Angle, and Linear Velocity \nTime, Radius, and Arc Length";
+				return "To calculate Angle, you need either of these sets of measurements:<p></p> <p>Angular Velocity and Time <p>Linear Velocity and Arc Length <p>Arc Length, Angular Velocity, and Linear Velocity <p>Time, Radius, and Linear Velocity</p></p></p></p>";
 			default:
 				return "";
 		}
--- AngularMotionSimulator/src/inputs/ErrorDialog.java
+++ AngularMotionSimulator/src/inputs/ErrorDialog.java
@@ -4,7 +4,6 @@
 package inputs;
 
 import javax.swing.*;
-
 import java.awt.*;
 
 /**
@@ -15,24 +14,24 @@ public class ErrorDialog extends JDialog {
 	private JFrame frame;
 	private JPanel panel;
 	private String message;
-	private JLabel text;
+	private JLabel label;
 
 	public ErrorDialog(JFrame jFrame, String errorMessage) {
 		super(jFrame, "Error", true);
 		this.frame = jFrame;
-		this.message = "<html>" + errorMessage + "</ALIGN></html>";
+		this.message = "<html>" + errorMessage + "</html>";
 		this.createComponents();
 		this.createDialog();
 	}
 
 	private void createComponents() {
 		panel = new JPanel(new BorderLayout());
-		panel.setPreferredSize(new Dimension(300, 100));
-		text = new JLabel(message, SwingConstants.CENTER);
+		panel.setPreferredSize(new Dimension(350, 125));
+		label = new JLabel(message, SwingConstants.CENTER);
 	}
 
 	private void createDialog() {
-		panel.add(text, BorderLayout.CENTER);
+		panel.add(label, BorderLayout.CENTER);
 		this.setContentPane(panel);
 		this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		this.pack();
