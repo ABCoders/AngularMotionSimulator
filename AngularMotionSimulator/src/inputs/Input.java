6c094e7bc0a90755f2724bb7a2fb25c0ca23995e
cindy-zhao <cindyzhaopan@hotmail.com>
2015-12-26 19:27:20 -0500

    comments

--- AngularMotionSimulator/src/inputs/Input.java
+++ AngularMotionSimulator/src/inputs/Input.java
@@ -5,28 +5,32 @@ import animation.Animation;
 import calculation.Calculations;
 
 public class Input extends Object{
-	public static final String[] VARIABLES = new String[] {"Angular Velocity",
+	public static final String[] VARIABLES = new String[] {"Angular Velocity", //A constant memory location for all variable names
 															"Linear Velocity",
 															"Radius",
 															"Arc Length",
 															"Time",
 															"Angle"};
-	private TreeMap<String, Double> variables;
+	private TreeMap<String, Double> variables; //Variables names and associated values
 	
-	private Animation animation;
-	private Calculations calculations;
-	private AnswerMachinePanel answerMachinePanel;
+	private Animation animation; //The Animation model
+	private Calculations calculations; //The Calculations model the Input is managing
+	private AnswerMachinePanel answerMachinePanel; //View for Input
 	
-	public static final int MIN_FIELDS = 2;
-	public static final int MAX_FIELDS = 5;
+	public static final int MIN_FIELDS = 2; //Minimum number of fields
+	public static final int MAX_FIELDS = 5; //Maximum number of fields
 	
-	private int numberPanels;
-	private int numberFields;
-	private int removedField;
+	private int numberPanels; //
+	private int numberFields; //The current number of input fields
+	private int removedField; //The location of the field being removed
 	
-	private String wantedVariable;
+	private String wantedVariable; //The variable the user wants to solve for
 	
-	public Input() {
+	/**
+	 * Main Constructor
+	 */
+	public Input() 
+	{
 		this.calculations = new Calculations(this);
 		this.animation = new Animation(calculations);
 		this.variables = new TreeMap<String, Double>();
@@ -38,7 +42,12 @@ public class Input extends Object{
 		this.numberFields = 2;
 	}
 	
-	public void setGUI(AnswerMachinePanel answerMachinePanel) {
+	/**
+	 * Sets GUI for Input
+	 * @param answerMachinePanel - GUI to set
+	 */
+	public void setGUI(AnswerMachinePanel answerMachinePanel) //Sets GUI
+	{
 		this.answerMachinePanel = answerMachinePanel;
 	}
 	
@@ -47,6 +56,9 @@ public class Input extends Object{
 		this.updateView();
 	}
 	
+	/**
+	 * Adds an extra input field
+	 */
 	public void addField() {
 		if (numberFields<MAX_FIELDS) {
 			numberFields++;
@@ -54,6 +66,10 @@ public class Input extends Object{
 		}
 	}
 	
+	/**
+	 * Removes an input field if number of fields is greater than 2
+	 * @param position - position of field to remove
+	 */
 	public void removeField(int position) {
 		if (numberFields>MIN_FIELDS) {
 			numberFields--;
@@ -111,30 +127,60 @@ public class Input extends Object{
 		this.variables.put("Angle", angle);
 	}
 
+	/**
+	 * Return number of panels
+	 * @return number of panels
+	 */
 	public int getNumberPanels() {
 		return this.numberPanels;
 	}
 	
+	/**
+	 * Returns number of current input fields
+	 * @return number of fields
+	 */
 	public int getNumberFields() {
 		return this.numberFields;
 	}
 	
+	/**
+	 * Returns location of field being removed
+	 * @return location of removed field
+	 */
 	public int getRemovedField() {
 		return removedField;
 	}
 	
+	/**
+	 * Returns the variable the user wants to solve for
+	 * @return wanted variable
+	 */
 	public String getWantedVariable() {
 		return this.wantedVariable;
 	}
 	
+	/**
+	 * Returns animation model of input
+	 * @return animation model
+	 */
 	public Animation getAnimation() {
 		return this.animation;
 	}
 	
+	/**
+	 * Returns calculations model of input
+	 * @return calculations model
+	 */
 	public Calculations getCalculations() {
 		return this.calculations;
 	}
 	
+	/**
+	 * Sets value of specified variable
+	 * @param index - the variable to set
+	 * @param value - the value to set
+	 * @return
+	 */
 	public boolean setVariableValue(int index,  double value) {
 		if(index > -1 && index < VARIABLES.length) {
 			variables.put(VARIABLES[index], value);
@@ -143,6 +189,11 @@ public class Input extends Object{
 		return false;
 	}
 	
+	/**
+	 * Returns value of variable
+	 * @param index - the variable to get value of 
+	 * @return value
+	 */
 	public double getVariableValue(int index) {
 		if(index > -1 && index < VARIABLES.length) {
 			return variables.get(VARIABLES[index]);
@@ -150,53 +201,98 @@ public class Input extends Object{
 		return 0;
 	}
 	
+	/**
+	 * Sets all variable values to 0
+	 */
 	public void resetVariables() {
 		for(int i=0; i<VARIABLES.length; i++) {
 			this.variables.put(VARIABLES[i], 0.0);
 		}
 	}
 	
+	/**
+	 * Returns radius of animation
+	 * @return radius
+	 */
 	public double getRadius() {
 		return this.variables.get("Radius");
 	}
 	
+	/**
+	 * Returns time of animation
+	 * @return time
+	 */
 	public double getTime() {
 		return this.variables.get("Time");
 	}
 	
+	/**
+	 * Returns angular velocity of animation
+	 * @return angular velocity
+	 */
 	public double getAngularVelocity() {
 		return this.variables.get("Angular Velocity");
 	}
 	
+	/**
+	 * Returns linear velocity of animation
+	 * @return linear velocity
+	 */
 	public double getLinearVelocity() {
 		return this.variables.get("Linear Velocity");
 	}
 	
+	/**
+	 * Returns arc length of animation
+	 * @return arc length
+	 */
 	public double getArcLength() {
 		return this.variables.get("Arc Length");
 	}
 	
+	/**
+	 * Returns angle of animation
+	 * @return angle
+	 */
 	public double getAngle() {
 		return this.variables.get("Angle");
 	}
 	
+	/**
+	 * Returns GUI of animation
+	 * @return answer machine GUI
+	 */
 	public AnswerMachinePanel getView() {
 		return answerMachinePanel;
 	}
 	
+	/**
+	 * Updates answer machine GUI
+	 */
 	private void updateView() {
 		answerMachinePanel.update();
 	}
 	
+	/**
+	 * 
+	 * @param state
+	 */
 	private void updateInputPanel(boolean state) {
 		InputPanel input = answerMachinePanel.getInputPanel();
 		input.update(state);
 	}
 	
+	/**
+	 * 
+	 */
 	private void updateWantedFieldPanel() {
 		answerMachinePanel.getWantedFieldPanel().update();
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public boolean updateModel() {
 		int numVar = 0;
 		for(int i=0; i<VARIABLES.length; i++) {
@@ -213,6 +309,9 @@ public class Input extends Object{
 		return false;
 	}
 	
+	/**
+	 * Updates calculations model
+	 */
 	public void updateCalculations() {
 		calculations.update();
 	}
