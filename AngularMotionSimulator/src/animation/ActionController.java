31eef4f4a7561bae5feecefd52fccc5396f11bb6
Kristinus <ben2616@hotmail.com>
2015-12-30 15:42:13 -0500

    test

--- AngularMotionSimulator/src/animation/ActionController.java
+++ AngularMotionSimulator/src/animation/ActionController.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionListener;
 
 /**
  * A Controller that listens to the button events on ActionPanel. 
- * Sends the data to the Animation model
+ * Sends the data to the Animation model.
  * @author BRYAN KRISTIONO
  * @since 12/12/2015
  */
@@ -13,7 +13,7 @@ public class ActionController implements ActionListener {
 	private Animation animation;		//The model used for calculation animation
 	
 	/**
-	 * Initializes a newly created listener for buttons in the ActionPanel
+	 * Initializes a newly created listener for buttons in the ActionPanel.
 	 * @param animation The model of the controller
 	 */
 	public ActionController (Animation animation) {
@@ -23,7 +23,7 @@ public class ActionController implements ActionListener {
 	/**
 	 * Sets values to the model after a button is clicked.
 	 * It changes the state of the animation.
-	 * It also resets the drawn point in the animation 
+	 * It also resets the drawn point in the animation.
 	 * @param e The event from the button
 	 */
 	@Override
